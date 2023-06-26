4c4b7510894cb8129b4a2dce7deaffbfbb51b3f5
osnick50 <oskar.nicz@gmail.com>
2023-06-26 13:37:42 +0200

    feat: added serial port connection for MAC and Win

--- main.py
+++ main.py
@@ -1,12 +1,18 @@
 import serial
+import sys
 from eeg_data_parser.eeg_parser import EegParser
 
-ser = serial.Serial('/dev/tty.BrainLink_Lite', 9600)
 parser = EegParser()
+
+if sys.platform.startswith('win'):
+    ser = serial.Serial(port='COM6')
+
+elif sys.platform.startswith('darwin'):
+    ser = serial.Serial('/dev/tty.BrainLink_Lite', 9600)
+
 while True:
     line = ser.readline()
     parser.parse_eeg_data(line)
-
 
 
 
